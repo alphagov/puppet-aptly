1e90b6f26fa3987beb31c563c89c821efe5079cc
iso <inny.so@digital.cabinet-office.gov.uk>
2014-05-07 12:24:16 +0100

    Add comment

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -2,14 +2,13 @@
 #
 # Create a repository using `aptly create`. It will not snapshot, or update the
 # repository for you, because it will take a long time and it doesn't make sense
-# to schedule these actions frequently in Puppet.
-#
-# NB: 
+# to schedule these actions frequently in Puppet. 
 #
 # === Parameters
 #
 # [*component*]
 #   Specify which component to put the package in. Default to 'main'
+#
 define aptly::repo(
   $component = 'main'
 ){
