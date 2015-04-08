9562647cf64ed24c7919594f02fc9d399b6fdd4d
William Hutson <wilrnh@msn.com>
2015-04-08 18:35:59 -0400

    Added $user and $group params to aptly::api

--- manifests/api.pp
+++ manifests/api.pp
@@ -8,6 +8,14 @@
 #   Ensure to pass on to service type
 #   Default: running
 #
+# [*user*]
+#   User to run the service as.
+#   Default: root
+#
+# [*group*]
+#   Group to run the service as.
+#   Default: root
+#
 # [*listen*]
 #   What IP/port to listen on for API requests.
 #   Default: ':8080'
@@ -18,11 +26,15 @@
 
 class aptly::api (
   $ensure         = running,
+  $user           = 'root',
+  $group          = 'root',
   $listen         = ':8080',
   $log            = 'none',
   ) {
     
     validate_re($ensure, ['^stopped|running$'], 'Valid values for $ensure: stopped, running')
+    
+    validate_string($user, $group)
     
     validate_re($listen, ['^[0-9.]*:[0-9]+$'], 'Valid values for $listen: :port, <ip>:<port>')
     
