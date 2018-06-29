f5488651ff05929da1ac2d22d31e7f86ed079e55
Chris Butler <chrisb@zedcore.com>
2018-06-29 11:15:42 +0100

    Make apt::source usage work with latest version
    Since version 2.0.0 of the puppetlabs/apt module, the "key_server"
    argument has been deprecated in favour of specifying a "key" hash. The
    key_server parameter was removed completely in version 4.0.0 of the
    module.
    
    This changeset updates the apt::source resource block to use the new
    style and bumps the supported version of the apt module up to include
    the 4.x branch.

--- manifests/init.pp
+++ manifests/init.pp
@@ -69,11 +69,13 @@ class aptly (
 
   if $repo {
     apt::source { 'aptly':
-      location   => 'http://repo.aptly.info',
-      release    => 'squeeze',
-      repos      => 'main',
-      key_server => $key_server,
-      key        => 'DF32BC15E2145B3FA151AED19E3E53F19C7DE460',
+      location => 'http://repo.aptly.info',
+      release  => 'squeeze',
+      repos    => 'main',
+      key      => {
+        id     => 'DF32BC15E2145B3FA151AED19E3E53F19C7DE460',
+        server => $key_server,
+      }
     }
 
     Apt::Source['aptly'] -> Class['apt::update'] -> Package['aptly']
