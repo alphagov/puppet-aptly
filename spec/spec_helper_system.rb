21a020d34d2584f651114a4a8b248cecfcfc3aa2
Mike Newton <mike.newton@bigcommerce.com>
2020-09-14 11:36:31 -0700

    bump puppetlabs-stdlib and puppetlabs-apt

--- spec/spec_helper_system.rb
+++ spec/spec_helper_system.rb
@@ -13,7 +13,7 @@ RSpec.configure do |c|
   c.before :suite do
     puppet_install
     puppet_module_install(source: proj_root, module_name: 'aptly')
-    shell('puppet module install puppetlabs-stdlib')
-    shell('puppet module install puppetlabs-apt --version 2.2.0')
+    shell('puppet module install puppetlabs-stdlib --version 6.4.0')
+    shell('puppet module install puppetlabs-apt --version 7.5.0')
   end
 end
